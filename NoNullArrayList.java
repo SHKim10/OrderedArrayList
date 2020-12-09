b7226ed7b2e37046d663cefb3825638de0e0611f
Stacy <skim10@stuy.edu>
2020-12-09 13:12:23 -0500

    Implemented set

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,4 +1,8 @@
 import java.util.ArrayList;
 public class NoNullArrayList<T> extends ArrayList<T>{
-  
+
+  public T set(int index, T element){
+    if (element == null) throw new IllegalArgumentException();
+    return super.set(index, element);
+  }
 }
