b6239242302ce546baf66ff71a514a00307983d2
Stacy <skim10@stuy.edu>
2020-12-13 17:58:39 -0500

    Added error messages

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -2,17 +2,17 @@ import java.util.ArrayList;
 public class NoNullArrayList<T> extends ArrayList<T>{
 
   public boolean add(T element){
-    if (element == null) throw new IllegalArgumentException();
+    if (element == null) throw new IllegalArgumentException("Null is an illegal element.");
     return super.add(element);
   }
 
   public void add(int index, T element){
-    if (element == null) throw new IllegalArgumentException();
+    if (element == null) throw new IllegalArgumentException("Null is an illegal element.");
     super.add(index, element);
   }
 
   public T set(int index, T element){
-    if (element == null) throw new IllegalArgumentException();
+    if (element == null) throw new IllegalArgumentException("Null is an illegal element.");
     return super.set(index, element);
   }
 }
