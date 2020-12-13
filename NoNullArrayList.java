b80d322ac23731c4cf3118724855e1e644e48165
Stacy <skim10@stuy.edu>
2020-12-13 17:57:05 -0500

    Implemented add methods

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,6 +1,16 @@
 import java.util.ArrayList;
 public class NoNullArrayList<T> extends ArrayList<T>{
 
+  public boolean add(T element){
+    if (element == null) throw new IllegalArgumentException();
+    return super.add(element);
+  }
+
+  public void add(int index, T element){
+    if (element == null) throw new IllegalArgumentException();
+    super.add(index, element);
+  }
+
   public T set(int index, T element){
     if (element == null) throw new IllegalArgumentException();
     return super.set(index, element);
