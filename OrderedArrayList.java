da3e12b19545eea169e2a4abe76402dc7fc3fc19
Stacy <skim10@stuy.edu>
2020-12-14 02:28:48 -0500

    Implemented add methods and private helper method

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,4 +1,22 @@
-import java.util.ArrayList;
 public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T>{
 
+  //Helper method for finding the correct index to add to
+  private int insertIndex(T element){
+    for (int i = 0; i < size(); i++){
+      if (element.compareTo(super.get(i)) < 0){
+        return i;
+      }
+    }
+    return size();
+  }
+
+  public boolean add(T element){
+    if (element == null) throw new IllegalArgumentException("Null is an illegal element.");
+    super.add(insertIndex(element), element);
+    return true;
+  }
+
+  public void add(int index, T element){
+    add(element);
+  }
 }
